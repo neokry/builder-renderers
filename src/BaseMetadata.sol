37f44881ce8a46c4a3b034644a092a1406df7c85
neokry <neokry.eth@gmail.com>
2023-11-15 15:16:07 +0700

    wip

--- src/BaseMetadata.sol
+++ src/BaseMetadata.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
-abstract contract BaseMetadata {
+import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+abstract contract BaseMetadata is Initializable {
     struct AdditionalTokenProperty {
         string key;
         string value;
@@ -10,11 +12,11 @@ abstract contract BaseMetadata {
 
     /// @custom:storage-location erc7201:nounsbuilder.storage.BaseMetadata
     struct BaseMetadataStorage {
-        address token;
-        string projectURI;
-        string description;
-        string contractImage;
-        AdditionalTokenProperty[] additionalTokenProperties;
+        address _token;
+        string _projectURI;
+        string _description;
+        string _contractImage;
+        AdditionalTokenProperty[] _additionalTokenProperties;
     }
 
     // keccak256(abi.encode(uint256(keccak256("nounsbuilder.storage.BaseMetadata")) - 1)) & ~bytes32(uint256(0xff))
@@ -26,7 +28,16 @@ abstract contract BaseMetadata {
         }
     }
 
-    function __BaseMetadata_init(string memory name_, string memory symbol_) internal {}
-
-    constructor() {}
+    function __BaseMetadata_init(
+        address token_,
+        string memory projectURI_,
+        string memory description_,
+        string memory contractImage_
+    ) internal onlyInitializing {
+        BaseMetadataStorage storage $ = _getBaseMetadataStorage();
+        $._token = token_;
+        $._projectURI = projectURI_;
+        $._description = description_;
+        $._contractImage = contractImage_;
+    }
 }
